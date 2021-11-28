fcb042723401dcf5688b58b9746949206924ced2
franss22 <franortizz2001@gmail.com>
2021-11-28 13:56:07 -0300

    googl-credentials

--- sheetTest.py
+++ sheetTest.py
@@ -7,32 +7,15 @@ import decimal as dec
 import os
 import json
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
-SERVICE_ACCOUNT_FILE = "keys.json"
+SERVICE_ACCOUNT_FILE = "google-credentials.json"
 creds = None
 
 
-print("generating config vars")
 
-if os.path.exists(SERVICE_ACCOUNT_FILE):
-    print("generating creds from file")
-    creds = service_account.Credentials.from_service_account_file(
-    SERVICE_ACCOUNT_FILE, scopes=SCOPES)
-else:
-    print("generating creds from config vars")
-    info = json.loads(str({ 
-    "type": "service_account",
-    "project_id": "sheets-333321",
-    "private_key_id": os.environ.get('private_key_id'),
-    "private_key": os.environ.get('private_key'),
-    "client_email": "costillabot@sheets-333321.iam.gserviceaccount.com",
-    "client_id": os.environ.get('client_id'),
-    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-    "token_uri": "https://oauth2.googleapis.com/token",
-    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/costillabot%40sheets-333321.iam.gserviceaccount.com"
-    }))
-    print(info)
-    creds = service_account.Credentials.from_service_account_info(info)
+print("generating creds from file")
+creds = service_account.Credentials.from_service_account_file(
+SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+
 
 
 
