d58f22222ba5cec374193fd6dc7e8376f58714ec
franss22 <franortizz2001@gmail.com>
2023-02-26 18:25:46 -0300

    xp must be an int

--- utils.py
+++ utils.py
@@ -5,6 +5,7 @@ def sign(num):
     return 1 if num >= 0 else -1
 
 def level_xp(xp:int):
+    xp = int(xp)
     xp_table = enumerate([0,300,900,2700,6500,14000,23000,34000,48000,64000,85000,100000,120000,140000,165000,195000,225000,265000,305000,355000])
     level = 20
     missing_xp = -1
